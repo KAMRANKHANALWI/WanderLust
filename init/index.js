027d5da1854bee395375b289918344e54bf52b77
Kamran Khan Alwi <beingkamrankhanalwi@gmail.com>
2024-05-31 00:50:03 +0530

    auth

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const intiDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "66582f3363dacac1bbb6893e"}))
     await Listing.insertMany(initData.data);
     console.log("data was initalized");
 };
